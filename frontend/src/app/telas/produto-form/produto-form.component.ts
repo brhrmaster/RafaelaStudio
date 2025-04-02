1dfcd5273cbe0762c7c84cb9c448c05d5133e1a1
Bruno Henry <henrydecarvalho@hotmail.com>
2025-04-02 01:50:20 -0300

    inteligencia da tela cadastro de produtos

--- frontend/src/app/telas/produto-form/produto-form.component.ts
+++ frontend/src/app/telas/produto-form/produto-form.component.ts
@@ -1,5 +1,6 @@
+import { valueOrDefault } from 'chart.js/helpers';
 import { Fornecedor, GetProdutoFormatosResponse } from './../../models/models.component';
-import { Component, EventEmitter, inject, Output, OnInit } from '@angular/core';
+import { Component, EventEmitter, inject, Output, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProdutoService } from '../../services/produto.service';
 import { Produto, ProdutoFormato } from '../../models/models.component';
@@ -14,7 +15,10 @@ import { FornecedorService } from '../../services/fornecedor.service';
     LoadingComponent
   ],
   templateUrl: './produto-form.component.html',
-  styles: ''
+  styleUrls: [
+    '../../styles/tela-form.css',
+    './produto-form.component.css'
+  ]
 })
 export class ProdutoFormComponent {
 
@@ -26,6 +30,10 @@ export class ProdutoFormComponent {
   fornecedorService: FornecedorService = inject(FornecedorService);
   produtoFormatos!: ProdutoFormato[];
   fornecedoresDisponiveis!: Fornecedor[];
+  fornecedoresSelecionados: Fornecedor[] = [];
+  @ViewChild('comboFornecedoresDisponiveis') comboFornecedoresDisponiveis!: ElementRef;
+
+  // TODO: aplicar mascara para valor: https://stackoverflow.com/questions/64364646/creating-directive-in-angular-that-formats-the-value-entered-on-keypress
 
   constructor() {
     this.obterFormatos();
@@ -34,6 +42,28 @@ export class ProdutoFormComponent {
 
   private showLoadingComponent(show: boolean) {
     this.isLoadingVisible = show;
+  }
+
+  incluirFornecedorSelecionado() {
+    const fornecedorId = this.comboFornecedoresDisponiveis.nativeElement.value;
+
+    for (let i=0; i < this.fornecedoresDisponiveis.length; i++) {
+      if (this.fornecedoresDisponiveis[i].id == fornecedorId) {
+        this.fornecedoresSelecionados.push(this.fornecedoresDisponiveis[i]);
+        this.fornecedoresDisponiveis.splice(i, 1);
+        break;
+      }
+    }
+  }
+
+  removerFornecedorSelecionado(fornecedor: Fornecedor) {
+    for (let i=0; i < this.fornecedoresSelecionados.length; i++) {
+      if (this.fornecedoresSelecionados[i].id == fornecedor.id) {
+        this.fornecedoresDisponiveis.push(this.fornecedoresSelecionados[i]);
+        this.fornecedoresSelecionados.splice(i, 1);
+        break;
+      }
+    }
   }
 
   obterFormatos() {
